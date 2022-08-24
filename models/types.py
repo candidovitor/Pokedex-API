d47b7e8a5102146493e08940f3266dfb9e8a160b
vitor candido <vitor.cr018@gmail.com>
2022-08-24 11:00:31 -0300

    creading type model

---
+++ models/types.py
@@ -0,0 +1,18 @@
+from database import db
+
+class TypesModel(db.Model):
+    __tabelaname__ = 'types'
+
+    type_id = db.Column(db.Integer, primary_key=True)
+    type_name = db.Column(db.String(20))
+
+    def __init__(self, type_id, type_name):
+        self.type_id = type_id
+        self.type_name = type_name
+
+    def json(self):
+        return {
+            'type_id': self.type_id,
+            'type_name': self.type_name
+        }
+
